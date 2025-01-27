9ccf35cbf656f55a3d3484e5aa2c911f244586fe
piop2 <pioooop2@gmail.com>
2025-01-27 14:43:09 +0900

    add animation direction: Forward, Reverse

--- pygame_animation/animation.py
+++ pygame_animation/animation.py
@@ -7,6 +7,12 @@ from typing import Optional
 from pygame import Surface
 
 from pygame_animation.timer import CountUpTimer
+from pygame_animation.direction import (
+    DirectionIterable,
+    DirectionIterator,
+    Forward,
+    Reverse,
+)
 
 
 @dataclass
@@ -33,20 +39,13 @@ class Animation:
     def __init__(
         self,
         frames: Optional[list[Frame]] = None,
-        repeat: int = -1,
-        direction: Optional[DirectionType] = None,
-        tags: Optional[dict[str, Tag]] = None
+        repeat: int = 0,
+        direction_type: Optional[DirectionType] = None,
+        tags: Optional[dict[str, Tag]] = None,
     ) -> None:
         if frames is None:
             frames = []
         self._frames: list[Frame] = frames
-
-        self._total_repeat: int = repeat
-        self._repeat: int = 0
-
-        if direction is None:
-            direction = DirectionType.FORWARD
-        self._direction_type: DirectionType = direction
 
         if tags is None:
             tags = {}
@@ -54,50 +53,48 @@ class Animation:
 
         self._timer: CountUpTimer = CountUpTimer()
 
-        self._index: int
-        self._direction: int
-        match direction:
-            case DirectionType.FORWARD | DirectionType.PING_PONG:
-                self._index = 0
-                self._direction = 1
-            case DirectionType.REVERSE | DirectionType.PING_PONG_REVERSE:
-                self._index = len(frames) - 1
-                self._direction = -1
+        if direction_type is None:
+            direction_type = DirectionType.FORWARD
+        direction_class: type[DirectionIterable]
+        match direction_type:
+            case DirectionType.FORWARD:
+                direction_class = Forward
+            case DirectionType.REVERSE:
+                direction_class = Reverse
             case _:
                 raise RuntimeError
+
+        self._direction: DirectionIterable = direction_class(
+            repeat=repeat, frame_length=len(frames)
+        )
+        self._direction_iterator: DirectionIterator = iter(self._direction)
+        self._index: int = next(self._direction_iterator)
         return
-    
+
     def get_frames(self) -> tuple[Frame]:
         return self._frames
-    
+
     def get_index(self) -> int:
         return self._index
-    
-    def get_total_repeat(self) -> int:
-        return self._total_repeat
-    
+
     def get_tags(self) -> tuple[Tag]:
         return tuple(self._tags.values())
-    
+
     def is_playing(self) -> bool:
         return self._timer.is_paused()
 
     def play(self) -> None:
         self._timer.unpause()
         return
-    
+
     def pause(self) -> None:
         self._timer.pause()
         return
-    
-    def reset(self) -> None:
-        ...
 
-    def split_by_tag(tag_name: str) -> Animation:
-        ...
-    
-    def update(self, ms: int) -> None:
-        ...
-    
-    def render(self) -> Surface:
-        ...
+    def reset(self) -> None: ...
+
+    def split_by_tag(tag_name: str) -> Animation: ...
+
+    def update(self, ms: int) -> None: ...
+
+    def render(self) -> Surface: ...
---
+++ pygame_animation/direction.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+
+from typing import Iterable, Iterator
+
+
+class DirectionIterable(Iterable):
+    def __init__(self, repeat: int, frame_length: int) -> None:
+        self._repeat: int = repeat
+        self._frame_length: int = frame_length
+        return
+
+    def __iter__(self) -> Iterator:
+        raise NotImplementedError
+
+
+class DirectionIterator(Iterator):
+    def __init__(self, repeat: int, frame_length: int) -> None:
+        super().__init__()
+        self._repeat: int = repeat
+        self._frame_length: int = frame_length
+        self._index: int
+        return
+
+    def __next__(self) -> int:
+        raise NotImplementedError
+
+
+class Forward(DirectionIterable):
+    def __iter__(self) -> ForwardIterator:
+        return ForwardIterator(self._repeat, self._frame_length)
+
+
+class ForwardIterator(DirectionIterator):
+    def __init__(self, repeat, frame_length) -> None:
+        super().__init__(repeat, frame_length)
+        self._index = 0
+        return
+
+    def __next__(self) -> int:
+        if self._frame_length == 0:
+            raise StopIteration
+
+        frame_index: int = self._index
+        if frame_index == self._frame_length:
+            self._repeat -= 1
+            if self._repeat == 0:
+                raise StopIteration
+
+            self._index = 0
+            return frame_index
+
+        self._index += 1
+        return frame_index
+
+
+class Reverse(DirectionIterable):
+    def __iter__(self) -> ReverseIterator:
+        return ReverseIterator(self._repeat, self._frame_length)
+
+
+class ReverseIterator(DirectionIterator):
+    def __init__(self, repeat, frame_length) -> None:
+        super().__init__(repeat, frame_length)
+        self._index = frame_length - 1
+        return
+
+    def __next__(self) -> int:
+        if self._frame_length == 0:
+            raise StopIteration
+
+        frame_index: int = self._index
+        if frame_index == 0:
+            self._repeat -= 1
+            if self._repeat == 0:
+                raise StopIteration
+
+            self._index = self._frame_length - 1
+            return frame_index
+
+        self._index -= 1
+        return frame_index
