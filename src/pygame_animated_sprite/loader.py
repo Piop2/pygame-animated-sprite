7fb250adfc97d62ebb3d462790aa4bac977db41f
piop2 <pioooop2@gmail.com>
2025-04-29 00:37:59 +0900

    add animated sprite loader

--- src/pygame_animated_sprite/loader.py
+++ src/pygame_animated_sprite/loader.py
@@ -1,28 +1,57 @@
-from typing import Protocol, Any
-from pathlib import Path
+from __future__ import annotations
+
+from typing import Optional, Protocol
 from os import PathLike
-import json
+from pathlib import Path
+from dataclasses import dataclass
 
-import pygame
-from pygame import Surface
-
-from .struct import Tag, Frame
-
-
-class BaseLoader(Protocol):
-    def __init__(self) -> None:
-        pass
-
-    def load_animation(self, path: str | Path | PathLike):
-        pass
+from .direction import DirectionIterable
+from .struct import Frame, Tag
 
 
-class AsepriteLoader(BaseLoader):
-    def __init__(self) -> None:
-        # TODO Aseprite export 옵션에 따라 그에 맞게 로드할 수 있게 옵션 추가
+class __Singleton(type):
+    __instance: Optional[__Singleton] = None
 
+    def __call__(cls, *args, **kwargs) -> any:
+        if cls.__instance is None:
+            cls.__instance = super().__call__(*args, **kwargs)
+        return cls.__instance
+
+
+class AnimatedSpritLoader(metaclass=__Singleton):
+    def __init__(
+        self,
+        path: str | PathLike,
+        protocol: Optional[AnimatedSpriteLoaderProtocol] = None,
+    ) -> None:
         super().__init__()
-        pass
 
-    def load_animation(self, path):
-        pass
+        self.__path: str | PathLike = path
+
+        if protocol is None:
+
+            class DefaultProtocol(AnimatedSpriteLoaderProtocol):
+                pass
+
+            protocol = DefaultProtocol()
+        self.__protocol: AnimatedSpriteLoaderProtocol = protocol
+        return
+
+    @property
+    def path(self) -> Path:
+        return self.__path
+
+    def load(self) -> LoadedAnimatedSprite:
+        return self.__protocol.load(self.__path)
+
+
+class AnimatedSpriteLoaderProtocol(Protocol):
+    def load(self, path: str | PathLike) -> LoadedAnimatedSprite: ...
+
+
+@dataclass(frozen=True)
+class LoadedAnimatedSprite:
+    frames: tuple[Frame, ...]
+    repeat: int
+    direction: DirectionIterable
+    tags: dict[str, Tag]
--- src/pygame_animated_sprite/struct.py
+++ src/pygame_animated_sprite/struct.py
@@ -23,5 +23,6 @@ class Tag:
 class Frame:
     image: Surface
     duration: int
+
     def copy(self) -> Frame:
         return Frame(self.image.copy(), self.duration)
