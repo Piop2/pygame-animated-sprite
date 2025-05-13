e7f0a4aaf80257b3ecb887caa7f435e299f7183c
piop2 <pioooop2@gmail.com>
2025-05-13 23:03:52 +0900

    change member's modify type in Direction classes

--- src/pygame_animated_sprite/direction.py
+++ src/pygame_animated_sprite/direction.py
@@ -2,28 +2,23 @@ from __future__ import annotations
 
 from typing import Iterable, Iterator
 
+
 class DirectionIterable(Iterable):
     def __init__(self, repeat: int, frame_length: int) -> None:
-        self.__repeat: int = repeat
-        self.__frame_length: int = frame_length
+        self.repeat: int = repeat
+        self.frame_length: int = frame_length
         return
 
     def __iter__(self) -> Iterator:
         raise NotImplementedError
 
-    def get_repeat(self) -> int:
-        return self.__repeat
-
-    def get_frame_length(self) -> int:
-        return self.__frame_length
-
 
 class DirectionIterator(Iterator):
-    def __init__(self, repeat: int, frame_length: int) -> None:
+    def __init__(self, repeat: int, frame_length: int, start_index: int = 0) -> None:
         super().__init__()
-        self._repeat: int = repeat
-        self._frame_length: int = frame_length
-        self._index: int
+        self.repeat: int = repeat
+        self.frame_length: int = frame_length
+        self.index: int = start_index
         return
 
     def __next__(self) -> int:
@@ -32,55 +27,55 @@ class DirectionIterator(Iterator):
 
 class Forward(DirectionIterable):
     def __iter__(self) -> ForwardIterator:
-        return ForwardIterator(self.get_repeat(), self.get_frame_length())
+        return ForwardIterator(self.repeat, self.frame_length)
 
 
 class ForwardIterator(DirectionIterator):
-    def __init__(self, repeat, frame_length) -> None:
-        super().__init__(repeat, frame_length)
-        self._index = 0
+    def __init__(self, repeat, frame_length, start_index=0) -> None:
+        super().__init__(repeat=repeat, frame_length=frame_length, start_index=0)
         return
 
     def __next__(self) -> int:
-        if self._frame_length == 0:
+        if self.frame_length == 0:
             raise StopIteration
 
-        frame_index: int = self._index
-        if frame_index == self._frame_length:
-            self._repeat -= 1
-            if self._repeat == 0:
+        frame_index: int = self.index
+        if frame_index == self.frame_length:
+            self.repeat -= 1
+            if self.repeat == 0:
                 raise StopIteration
 
-            self._index = 0
+            self.index = 0
             return frame_index
 
-        self._index += 1
+        self.index += 1
         return frame_index
 
 
 class Reverse(DirectionIterable):
     def __iter__(self) -> ReverseIterator:
-        return ReverseIterator(self.get_repeat(), self.get_frame_length())
+        return ReverseIterator(self.repeat, self.frame_length)
 
 
 class ReverseIterator(DirectionIterator):
-    def __init__(self, repeat, frame_length) -> None:
-        super().__init__(repeat, frame_length)
-        self._index = frame_length - 1
+    def __init__(self, repeat, frame_length, start_index=0) -> None:
+        super().__init__(
+            repeat=repeat, frame_length=frame_length, start_index=frame_length - 1
+        )
         return
 
     def __next__(self) -> int:
-        if self._frame_length == 0:
+        if self.frame_length == 0:
             raise StopIteration
 
-        frame_index: int = self._index
+        frame_index: int = self.index
         if frame_index == 0:
-            self._repeat -= 1
-            if self._repeat == 0:
+            self.repeat -= 1
+            if self.repeat == 0:
                 raise StopIteration
 
-            self._index = self._frame_length - 1
+            self.index = self.frame_length - 1
             return frame_index
 
-        self._index -= 1
+        self.index -= 1
         return frame_index
