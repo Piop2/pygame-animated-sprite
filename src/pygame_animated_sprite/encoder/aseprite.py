4769a384bffc5e0899c53d367d3e21c0a801375c
piop2 <pioooop2@gmail.com>
2025-05-17 20:20:23 +0900

    implement AsepriteEncoder

--- src/pygame_animated_sprite/encoder/aseprite.py
+++ src/pygame_animated_sprite/encoder/aseprite.py
@@ -1,19 +1,111 @@
 from __future__ import annotations
 
+from typing import Literal, TypedDict
 from pathlib import Path
+import json
 
+import pygame.image
+from pygame import Surface
+
+from ..direction import DirectionIterable, Forward, Reverse
+from ..struct import Frame, Tag
 from .base import AnimatedSpriteEncoder, AnimatedSpriteData
 
 
-# TODO Aseprite EXPORT FILE 인코더입니다
+__Size = TypedDict("__Size", {"w": int, "h": int})
+__Rect = TypedDict("__Rect", {"x": int, "y": int, "w": int, "h": int})
+__Frames = TypedDict(
+    "__Frames",
+    {
+        "frame": __Rect,
+        "rotated": bool,
+        "trimmed": bool,
+        "spriteSourceSize": __Rect,
+        "sourceSize": __Size,
+    },
+)
+__Tag = TypedDict(
+    "__Tag",
+    {
+        "name": str,
+        "from": int,
+        "to": int,
+        "direction": Literal["forward", "reverse", "pingpong", "pingpong_reverse"],
+        "color": str,
+    },
+)
+__Meta = TypedDict(
+    "__Meta",
+    {
+        "app": str,
+        "version": str,
+        "image": str,
+        "format": str,
+        "size": __Size,
+        "scale": str,
+        "frameTags": list[__Tag],
+    },
+)
+
 
 class AsepriteEncoder(AnimatedSpriteEncoder):
-    """Aseprite exported file encoder"""
-    def __init__(self) -> None:
+    """Aseprite sprite sheet encoder"""
+
+    def __init__(self, json_type: Literal["array", "hash"], tags: bool = False) -> None:
+        self.json_type: Literal["array", "hash"] = json_type
+
+        # meta
+        self.tags: bool = tags
+        # self.layers
+        # self.slices
         return
 
     def load_file(self, path: Path) -> AnimatedSpriteData:
-        pass
+        if path.suffix != ".json":
+            return
+
+        frames: list[Frame] = []
+        tags: list[Tag] = []
+
+        data: dict[str, any] = json.load(path.as_posix)
+        # ---------meta---------
+        meta_data: __Meta = data["meta"]
+
+        packed_image: Surface = pygame.image.load(f"{path.parent}/{meta_data['image']}")
+        for tag_data in meta_data["frameTags"]:
+            tag_direction: DirectionIterable
+            match tag_data["direction"]:
+                case "forward":
+                    tag_direction = Forward
+                case "reverse":
+                    tag_direction = Reverse
+                case "pingpong":
+                    pass
+                case "pingpong_reverse":
+                    pass
+                case _:
+                    NotImplementedError(
+                        f"not implemented direction: {tag_data['direction']}"
+                    )
+
+            tags.append(
+                Tag(
+                    tag_data["name"],
+                    tag_data["from"],
+                    tag_data["to"] + 1,
+                    tag_direction
+                )
+            )
+
+        # --------frames--------
+        frame_list: list[dict[str, any]]
+        if self.json_type == "hash":
+            frame_list = list(data["frames"].values())
+
+        frame_data: __Frames
+        for frame_data in frame_list:
+            frame_rect: __Rect = frame_data["frame"]
+            duration: int = frame_data["duration"]
 
     def load_folder(self, path: Path) -> AnimatedSpriteData:
         pass
