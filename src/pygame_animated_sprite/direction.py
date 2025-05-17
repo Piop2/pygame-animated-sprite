2f1fbfa6247f2f780097785272bae58d58f097bf
piop2 <pioooop2@gmail.com>
2025-05-17 21:23:42 +0900

    add PingPong direction class

--- src/pygame_animated_sprite/direction.py
+++ src/pygame_animated_sprite/direction.py
@@ -42,7 +42,7 @@ class ForwardIterator(DirectionIterator):
         frame_index: int = self.index
         if frame_index == self.frame_length:
             self.repeat -= 1
-            if self.repeat == 0:
+            if self.repeat < 0:
                 raise StopIteration
 
             self.index = 0
@@ -59,9 +59,7 @@ class Reverse(DirectionIterable):
 
 class ReverseIterator(DirectionIterator):
     def __init__(self, repeat, frame_length) -> None:
-        super().__init__(
-            repeat=repeat, frame_length=frame_length
-        )
+        super().__init__(repeat=repeat, frame_length=frame_length)
         self.index = frame_length - 1
         return
 
@@ -72,11 +70,48 @@ class ReverseIterator(DirectionIterator):
         frame_index: int = self.index
         if frame_index == 0:
             self.repeat -= 1
-            if self.repeat == 0:
+            if self.repeat < 0:
                 raise StopIteration
 
             self.index = self.frame_length - 1
             return frame_index
 
         self.index -= 1
+        return frame_index
+
+
+class PingPong(DirectionIterable):
+    def __init__(self, repeat, frame_length) -> None:
+        super().__init__(repeat, frame_length)
+        return
+
+    def __iter__(self) -> PingPongIterator:
+        return PingPongIterator(self.repeat, self.frame_length)
+
+
+class PingPongIterator(DirectionIterator):
+    def __init__(self, repeat, frame_length) -> None:
+        super().__init__(repeat, frame_length)
+        self._direction: int = 1
+        return
+
+    def __next__(self) -> int:
+        if self.frame_length == 0:
+            raise StopIteration
+
+        frame_index: int = self.index
+        if frame_index == -1 and self._direction == -1:
+            self.repeat -= 1
+            if self.repeat < 0:
+                raise StopIteration
+
+            self.index = 0
+            return frame_index
+
+        if frame_index == -1 and self._direction == -1:
+            self._direction = 1
+        if frame_index == self.frame_length - 1 and self._direction == 1:
+            self._direction = -1
+
+        self.index += self._direction
         return frame_index
