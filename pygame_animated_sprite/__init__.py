c23e47b8337ef5958a9c2d84d77da3d2a6dc4d79
piop2 <pioooop2@gmail.com>
2025-06-30 23:21:41 +0900

    update version

--- pygame_animated_sprite/__init__.py
+++ pygame_animated_sprite/__init__.py
@@ -3,7 +3,7 @@ from pygame_animated_sprite.sprite import AnimatedSprite
 from pygame_animated_sprite.direction import Forward, Reverse
 from pygame_animated_sprite.struct import Frame, Tag
 
-__version__: str = "1.2.3"
+__version__: str = "1.3.0"
 __author__: str = "Pio"
 __email__: str = "pioooop2@gmail.com"
 __license__: str = "MIT"
