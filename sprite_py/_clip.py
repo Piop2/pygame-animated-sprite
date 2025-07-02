cd08a111b878facd05949792233f55359f927a89
piop2 <pioooop2@gmail.com>
2025-07-03 00:23:46 +0900

    add ImageClipper protocol class

--- sprite_py/_clip.py
+++ sprite_py/_clip.py
@@ -1,1 +1,9 @@
-# ImageClipper
+from __future__ import annotations
+
+from typing import Protocol
+
+from sprite_py._texture import TextureProtocol
+
+
+class ImageClipper(Protocol):
+    def clip(self, x: int, y: int, width: int, height: int) -> TextureProtocol: ...
