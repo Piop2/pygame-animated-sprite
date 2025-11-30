42ca46819cdaef400c305b2ccd6f775594c6b3fb
piop2 <pioooop2@gmail.com>
2025-11-30 15:28:29 +0900

    update version: 1.5.1

--- pygame_animated_sprite/__init__.py
+++ pygame_animated_sprite/__init__.py
@@ -8,7 +8,7 @@ from pygame_animated_sprite.direction import Forward, Reverse, PingPong, PingPon
 
 # from pygame_animated_sprite.structures import Frame, Tag
 
-__version__: str = "1.4.1"
+__version__: str = "1.5.1"
 __author__: str = "Pio"
 __email__: str = "pioooop2@gmail.com"
 __license__: str = "MIT"
